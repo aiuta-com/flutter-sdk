c6c61a492a120379d8249cbcc91e311d123bd664
MishkaV <mavorozhtsov_1@edu.hse.ru>
2024-10-16 13:50:03 +0200

    Move libs.version.toml for dependabot

--- example/android/settings.gradle.kts
+++ example/android/settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            from(files("../../gradle/libs.versions.toml"))
+            from(files("../../android/gradle/libs.versions.toml"))
         }
     }
 }
